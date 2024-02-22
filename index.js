70f5a5d2104c44addbf9758f05b137e7f8acabce
Matham <matham@Badars-MacBook-Air.local>
2024-02-22 13:21:13 +0400

    This is the first files for weather app

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.post("/submit", async (req, res) => {
+    try{
+        const response = await axios.get("https://api.open-meteo.com/v1/forecast?latitude=21,23.75&longitude=57,54.5&hourly=temperature_2m&forecast_days=1");
+        const result = response.data;
+        const time = result[req.body["country"]].hourly.time;
+        const temperature = result[req.body["country"]].hourly.temperature_2m;
+
+        res.render("index.ejs", {data: result,time: time, temperature: temperature});
+        console.log(req.body["country"]);
+
+    } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs");
+  }
+});
+
+app.listen(port, () => {
+   console.log(`Server is running on port ${port}`); 
+});
